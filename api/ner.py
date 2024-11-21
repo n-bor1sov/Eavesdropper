e6313185510062ac3ba3b0942f1535d868859da5
Nit31 <dydalinnnov31@gmail.com>
2024-11-21 19:10:44 +0300

    Streamlit app works now

--- api/ner.py
+++ api/ner.py
@@ -6,7 +6,8 @@ from pydantic import BaseModel
 import re
 app = FastAPI()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 with open('./models/label_map.pickle', 'rb') as f:
     label_to_idx = pickle.load(f)
 idx_to_label = {idx: label for label, idx in label_to_idx.items()}
@@ -23,33 +24,10 @@ class NERResponse(BaseModel):
     labels: list
 
 
-def clean_text(input_text):
-    """
-    Removes lines containing only numbers from the input text.
-
-    Args:
-        input_text (str): The text to be cleaned.
-
-    Returns:
-        str: The cleaned text with number-only lines removed.
-    """
-    # Split the input text into lines
-    lines = input_text.split('\n')
-
-    # Filter out lines that contain only numbers (possibly with leading/trailing whitespace)
-    cleaned_lines = [line for line in lines if not re.match(r'^\s*\d+\s*$', line)]
-
-    # Join the cleaned lines back into a single string
-    cleaned_text = '\n'.join(cleaned_lines)
-    return cleaned_text
-
-
 @app.post("/ner", response_model=NERResponse)
 async def get_ner(request: NERRequest):
     tokens_all, labels_all = [], []
-    text_without_timestamps = re.sub(r'\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}', '', request.text)
-    cleaned_text = clean_text(re.sub(r'^\d$\n', '', text_without_timestamps, flags=re.MULTILINE))
-    for chunk in chunked_text(cleaned_text):
+    for chunk in chunked_text(request.text):
         res = inference(chunk, model, tokenizer, idx_to_label, device)
         for token, label in res:
         #decoded_labels = [idx_to_label[label] for label in res]
--- front-end/app.py
+++ front-end/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import sys
 import os
+import time
 # Add the scripts directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'scripts')))
 
@@ -14,14 +15,66 @@ st.title("Audio Transcription App")
 uploaded_file = st.file_uploader("Choose an audio file", type=["wav", "mp3", "flac"])
 
 # Function to send audio file to the API and get transcription
+
+
 def transcribe_audio(file):
-    api_url = ""
-    files = {"file": file}
-    response = requests.post(api_url, files=files)
+    def create_task(file):
+        url = "http://127.0.0.1:8080/tasks/"
+        response = requests.post(url, files={'file': file})
+        if response.status_code == 200:
+            task_id = response.json()['task_id']
+            return task_id
+        else:
+            return None
+
+    task_id = create_task(file)
+    assert task_id is not None, "Task creation failed"
+
+    def get_task_status(task_id):
+        url = f"http://127.0.0.1:8080/tasks/{task_id}"
+        response = requests.get(url)
+        print(response.json())
+        return response.json()
+
+    response = get_task_status(task_id)
+    while response['status'] == 'in_progress':
+        response = get_task_status(task_id)
+
+        if 'status' not in response:
+            break
+
+        if response['status'] == 'failed':
+            # exception
+            raise Exception("Transcription failed")
+
+        time.sleep(1)
+
+    result_string = ""
+    for i, segment in enumerate(response['segments']):
+        result_string += f"Speaker {segment['speaker'][-2:]}: {segment['text'].strip()}\n"
+
+    return result_string
+
+
+def ner(result_string):
+    # Define the URL of the FastAPI app
+    url = "http://127.0.0.1:8081/ner"
+
+    # Define the input payload
+    payload = {
+        "text": result_string
+    }
+
+    # Send a POST request
+    response = requests.post(url, json=payload)
+
+    # Check the response
     if response.status_code == 200:
-        return response.json().get("transcription", "No transcription available")
+        print("Cool")
     else:
-        return f"Error: {response.status_code} - {response.text}"
+        print("Error:", response.status_code, response.text)
+
+    return response
 
 def summarize(ner_result):
     masked_text, key = mask([ner_result])
@@ -40,12 +93,18 @@ def summarize(ner_result):
     else:
         st.error(f"Error: {response.status_code} - {response.text}")
         return None
+
 # Button to trigger transcription
 if st.button("Transcribe"):
     if uploaded_file is not None:
         with st.spinner("Transcribing audio..."):
             transcription = transcribe_audio(uploaded_file)
-        st.subheader("Transcription:")
-        st.write(transcription)
+        with st.spinner("NER..."):
+            ner_result = ner(transcription).json()
+        with st.spinner("Summarizing..."):
+            summary = summarize(ner_result)
+        print(summary)
+        st.subheader("Summary:")
+        st.write(summary)
     else:
         st.error("Please upload an audio file first.")
